8975a7bce79dce1e7e2effddf73a85eff1e68d06
Pesandi Pabasara Munasinghe <munasinghepesandi@gmail.com>
2026-01-10 19:25:51 +0530

    Add Database class for MySQL connection management
    Implemented Database class for handling MySQL connections and queries.

---
+++ src/components/sections/connection.php
@@ -0,0 +1,27 @@
+<?php
+
+class Database {
+    public static $connection;
+
+    public static function setupConnection(){
+        if(isset(Database::$connection)){
+            Database::$connection = new mysqli("localhost", "root", "******", "pm_technologies","3306");
+        }
+    }
+
+        
+    public static function iud($q){
+        Database::setupConnection();
+        $resultset = Database::$connection->query($q);
+        
+    }
+
+    public static function search($q){
+        Database::setupConnection();
+        $resultset = Database::$connection->query($q);
+        return $resultset;
+    }
+}
+
+?>        
+<ContactForm />
